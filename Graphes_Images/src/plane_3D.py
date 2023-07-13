592e56a0cc6ac8c575591db24e1e70045c0d18df
FabianBraun121 <59761831+FabianBraun121@users.noreply.github.com>
2023-07-13 10:59:52 +0200

    Koala speed

--- Graphes_Images/src/plane_3D.py
+++ Graphes_Images/src/plane_3D.py
@@ -49,15 +49,65 @@ def plot_3d(array_2d):
 
     plt.show()
     
-def subtract_plane(field, plane_degree):
+def plot_images_without_planes(array_2d_top, array_2d_1, array_2d_2, array_2d_3):
+    # Create a figure for holding the plots
+    fig = plt.figure(figsize=(12, 8))
+
+    # create x and y values
+    x = np.arange(array_2d_top.shape[1])
+    y = np.arange(array_2d_top.shape[0])
+    x, y = np.meshgrid(x, y)
+
+    # Top subplot
+    ax_top = fig.add_subplot(2, 3, 2, projection='3d')
+    ax_top.plot_surface(x, y, array_2d_top, cmap='viridis')
+    ax_top.set_title('original phase image', pad=-20)
+    ax_top.set_xlabel('X')
+    ax_top.set_ylabel('Y')
+    ax_top.set_zlabel('Z')
+
+    # First bottom subplot
+    ax1 = fig.add_subplot(2, 3, 4, projection='3d')
+    ax1.plot_surface(x, y, array_2d_1, cmap='viridis')
+    ax1.set_title('3rd-degree', pad=-20)
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+
+    # Second bottom subplot
+    ax2 = fig.add_subplot(2, 3, 5, projection='3d')
+    ax2.plot_surface(x, y, array_2d_2, cmap='viridis')
+    ax2.set_title('4th-degree', pad=-20)
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+    ax2.set_zlabel('Z')
+
+    # Third bottom subplot
+    ax3 = fig.add_subplot(2, 3, 6, projection='3d')
+    ax3.plot_surface(x, y, array_2d_3, cmap='viridis')
+    ax3.set_title('6th-degree', pad=-20)
+    ax3.set_xlabel('X')
+    ax3.set_ylabel('Y')
+    ax3.set_zlabel('Z')
+
+    # Adjust layout to be tight
+    fig.tight_layout()
+
+    # Show the figure with the plots
+    plt.show()
+
+def claculate_plane(field, plane_degree):
     X1, X2 = np.mgrid[:field.shape[0], :field.shape[1]]
     X = np.hstack((X1.reshape(-1,1) , X2.reshape(-1,1)))
     X = PolynomialFeatures(degree=plane_degree, include_bias=False).fit_transform(X)
+    print(f'{plane_degree}: {X.shape[1]}')
     y = field.reshape(-1)
     reg = LinearRegression().fit(X, y)
     plane = reg.predict(X).reshape(field.shape[0],field.shape[1])
-    return field - plane
+    return plane
 
+def subtract_plane(field, plane_degree):
+    return field - claculate_plane(field, plane_degree)
 
 base_path = r'C:\Users\SWW-Bc20\Documents\GitHub\Imaging-pipeline-for-DHM\Graphes_Images'
 holo_path = base_path + r'data\C11_20230217\2023-02-17 11-13-34\00001\00001_00001\Holograms\00000_holo.tif'
@@ -78,5 +128,4 @@ image = np.load(save_path + os.sep + 'image.npy')
 #%%
 plot_3d(image)
 #%%
-plane = image - subtract_plane(image, 5)
-plot_3d(subtract_plane(image, 6))
+plot_images_without_planes(image, subtract_plane(image, 3), subtract_plane(image, 4), subtract_plane(image, 5))
